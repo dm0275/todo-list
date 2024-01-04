62238cde6b02befc9f6417d284ae7fb8a5a9d81f
Daniel Mancilla <dm0275@gmail.com>
2024-01-04 16:21:48 -0600

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Daniel Mancilla on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
